4017ef8ab9d5dde96294923f583b10943e3d1ac8
parkyountaek <yountaek11@gmail.com>
2023-04-11 21:30:38 +0900

    feat: userRepository 추가 및 테스트 코드 추가

--- borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/AppUserManagementApplication.java
+++ borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/AppUserManagementApplication.java
@@ -1,2 +1,16 @@
-package com.borrow.system.appusermanagement;public class AppUserManagementApplication {
+package com.borrow.system.appusermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan(basePackages = {"com.borrow.system.modulecore.user.*"})
+@SpringBootApplication(scanBasePackages = {
+        "com.borrow.system.modulecore.*",
+        "com.borrow.system.appusermanagement.*"
+})
+public class AppUserManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppUserManagementApplication.class, args);
+    }
 }
--- borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/domain/repsitory/UserRepository.java
+++ borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/domain/repsitory/UserRepository.java
@@ -1,2 +1,7 @@
-package com.borrow.system.appusermanagement.domain.repsitory;public interface UserRepository {
+package com.borrow.system.appusermanagement.domain.repsitory;
+
+import com.borrow.system.modulecore.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
