57388c1b55686d0d89f380674e9e7285d79716aa
박윤택 <pyt@bag-yuntaeg-ui-MacBookAir.local>
2023-05-08 22:19:09 +0900

    feat: Category domain edit

--- borrow-system/module-core/src/main/java/com/borrow/system/modulecore/category/domain/Category.java
+++ borrow-system/module-core/src/main/java/com/borrow/system/modulecore/category/domain/Category.java
@@ -1,6 +1,7 @@
 package com.borrow.system.modulecore.category.domain;
 
 import com.borrow.system.modulecore.audit.CreateBaseEntity;
+import com.borrow.system.modulecore.user.domain.User;
 import jakarta.persistence.*;
 
 @Entity
@@ -15,19 +16,23 @@ public class Category extends CreateBaseEntity {
     private int borrowDay;
     @Column(name = "IS_QR")
     private boolean isQr;
+    @ManyToOne
+    @JoinColumn(name = "CREATED_BY")
+    private User user;
 
     public Category() {
     }
 
-    public Category(Long id, String name, int borrowDay, boolean isQr) {
+    public Category(Long id, String name, int borrowDay, boolean isQr, User user) {
         this.id = id;
         this.name = name;
         this.borrowDay = borrowDay;
         this.isQr = isQr;
+        this.user = user;
     }
 
-    public static Category create(Long id, String name, int borrowDay, boolean isQr) {
-        return new Category(id, name, borrowDay, isQr);
+    public static Category create(Long id, String name, int borrowDay, boolean isQr, User user) {
+        return new Category(id, name, borrowDay, isQr, user);
     }
 
     public Long getId() {
