4c2a789fd4538310da2e7bafb95980271d11ab98
parkyountaek <yountaek11@gmail.com>
2023-05-11 00:11:31 +0900

    feat: UserPersistenceAdapter

--- borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/adapter/persistence/UserPersistenceAdapter.java
+++ borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/adapter/persistence/UserPersistenceAdapter.java
@@ -2,27 +2,31 @@ package com.borrow.system.appusermanagement.adapter.persistence;
 
 import java.util.Optional;
 
+import com.borrow.system.modulecommon.exception.BusinessLogicException;
+import com.borrow.system.modulecommon.exception.ExceptionCode;
 import org.springframework.stereotype.Repository;
 
 import com.borrow.system.appusermanagement.application.port.out.LoadUserCase;
 import com.borrow.system.modulecore.user.domain.User;
 
 @Repository
-public class UserPersistenceAdapterCase implements LoadUserCase {
+public class UserPersistenceAdapter implements LoadUserCase {
     private final UserRepository userRepository;
 
-    public UserPersistenceAdapterCase(UserRepository userRepository) {
+    public UserPersistenceAdapter(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    public User getUser(Long id) {
+    public User getUserById(Long id) {
         Optional<User> optionalUser = this.userRepository.findById(id);
-        return optionalUser.orElse(null);
+        return optionalUser.orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
     }
 
-    public Optional<User> findByEmail(String email) {
-        return this.userRepository.findByEmail(email);
+    @Override
+    public User getUserByEmail(String email) {
+        Optional<User> optionalUser = this.userRepository.findByEmail(email);
+        return optionalUser.orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
     }
 
     public User createUser(User user) {
