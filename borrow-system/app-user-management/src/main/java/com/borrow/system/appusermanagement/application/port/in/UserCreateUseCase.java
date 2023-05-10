5211b46c68d83922eef9525c1cd44cecbedab0ec
parkyountaek <yountaek11@gmail.com>
2023-05-11 00:10:27 +0900

    feat: 회원 생성 케이스

--- borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/application/port/in/UserCreateUseCase.java
+++ borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/application/port/in/UserCreateUseCase.java
@@ -2,6 +2,6 @@ package com.borrow.system.appusermanagement.application.port.in;
 
 import com.borrow.system.modulecore.user.domain.User;
 
-public interface CreateUseCase {
-    User createUser();
+public interface UserCreateUseCase {
+    User createUser(User user);
 }
