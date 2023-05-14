e4f7a2739cdce7a2cea63c730b12b90178458d24
parkyountaek <yountaek11@gmail.com>
2023-05-14 21:08:45 +0900

    feat: UserService 서비스 로직 추가

--- borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/application/service/UserService.java
+++ borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/application/service/UserService.java
@@ -1,10 +1,8 @@
 package com.borrow.system.appusermanagement.application.service;
 
-import java.util.Optional;
-
-import com.borrow.system.appusermanagement.application.port.in.AdminCreateUseCase;
-import com.borrow.system.modulecommon.exception.BusinessLogicException;
-import com.borrow.system.modulecommon.exception.ExceptionCode;
+import com.borrow.system.appusermanagement.application.port.in.UserUpdateUseCase;
+import com.borrow.system.appusermanagement.application.port.out.UserGetUseCase;
+import com.borrow.system.appusermanagement.util.CustomBeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.borrow.system.appusermanagement.adapter.persistence.UserPersistenceAdapter;
@@ -12,7 +10,7 @@ import com.borrow.system.appusermanagement.application.port.in.UserCreateUseCase
 import com.borrow.system.modulecore.user.domain.User;
 
 @Service
-public class UserService implements UserCreateUseCase, AdminCreateUseCase {
+public class UserService implements UserCreateUseCase, UserUpdateUseCase, UserGetUseCase {
     private final UserPersistenceAdapter userPersistenceAdapter;
 
     public UserService(UserPersistenceAdapter userPersistenceAdapter) {
@@ -24,18 +22,30 @@ public class UserService implements UserCreateUseCase, AdminCreateUseCase {
     }
 
     public void existUser(String email) {
-        this.userPersistenceAdapter.getUserByEmail(email);
-    }
-
-    @Override
-    public User createAdmin(User user) {
-        existUser(user.getEmail());
-        return this.userPersistenceAdapter.createUser(user);
+        this.userPersistenceAdapter.existUserByEmail(email);
     }
 
     @Override
     public User createUser(User user) {
         existUser(user.getEmail());
-        return this.userPersistenceAdapter.createUser(user);
+        return this.userPersistenceAdapter.saveUser(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        User findUser = findVerifiedUser(user.getEmail());
+        findUser.updateProperty(user);
+
+        return this.userPersistenceAdapter.saveUser(findUser);
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return this.userPersistenceAdapter.getUserByEmail(email);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return this.userPersistenceAdapter.getUserById(id);
     }
 }
