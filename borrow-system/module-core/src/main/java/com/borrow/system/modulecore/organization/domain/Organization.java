b0db3e25c438ba8752150d5eb25f7fa9471491b2
parkyountaek <yountaek11@gmail.com>
2023-05-14 21:53:11 +0900

    feat: Organization 도메인 생성

--- borrow-system/module-core/src/main/java/com/borrow/system/modulecore/organization/domain/Organization.java
+++ borrow-system/module-core/src/main/java/com/borrow/system/modulecore/organization/domain/Organization.java
@@ -1,2 +1,56 @@
-package com.borrow.system.modulecore.organization.domain;public class Organization {
+package com.borrow.system.modulecore.organization.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "BR_ORGANIZATION")
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "ADDRESS")
+    private String address;
+    @Column(name = "DETAIL_ADDRESS")
+    private String detailAddress;
+    @Column(name = "REPRESENTATIVE_NUMBER")
+    private String representativeNumber;
+    @Column(name = "FAX_NUMBER")
+    private String faxNumber;
+
+    public Organization() {
+    }
+
+    public Organization(String name, String address, String detailAddress, String representativeNumber, String faxNumber) {
+        this.name = name;
+        this.address = address;
+        this.detailAddress = detailAddress;
+        this.representativeNumber = representativeNumber;
+        this.faxNumber = faxNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDetailAddress() {
+        return detailAddress;
+    }
+
+    public String getRepresentativeNumber() {
+        return representativeNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
 }
