efa9dc846fee9df76842cb99ca75ba1d5360577b
parkyountaek <yountaek11@gmail.com>
2023-05-14 21:52:47 +0900

    feat: organization 연관관계 매핑

--- borrow-system/module-core/src/main/java/com/borrow/system/modulecore/user/domain/User.java
+++ borrow-system/module-core/src/main/java/com/borrow/system/modulecore/user/domain/User.java
@@ -1,6 +1,7 @@
 package com.borrow.system.modulecore.user.domain;
 
 import com.borrow.system.modulecore.audit.UpdateBaseEntity;
+import com.borrow.system.modulecore.organization.domain.Organization;
 import jakarta.persistence.*;
 
 import java.util.Objects;
@@ -18,15 +19,17 @@ public class User extends UpdateBaseEntity {
     private String name;
     @Column(name = "PASSWORD", nullable = false)
     private String password;
-    @Column(name = "ORGANIZATION", nullable = false)
-    private String organization;
     @Column(name = "PHONE_NUMBER", nullable = false)
     private String phoneNumber;
     @Enumerated(EnumType.STRING)
     @Column(name = "ROLE", nullable = false)
     private Role role;
 
-    private User(String email, String name, String password, String organization, String phoneNumber, Role role) {
+    @ManyToOne
+    @JoinColumn(name = "ORGANIZATION_ID")
+    private Organization organization;
+
+    private User(String email, String name, String password, Organization organization, String phoneNumber, Role role) {
         this.email = email;
         this.name = name;
         this.password = password;
@@ -37,17 +40,17 @@ public class User extends UpdateBaseEntity {
         this.initUpdateBaseEntity();
     }
 
-    public static User user(String email, String name, String password, String organization, String phoneNumber) {
+    public static User user(String email, String name, String password, Organization organization, String phoneNumber) {
         return new User(email, name, password, organization, phoneNumber, Role.USER);
     }
 
-    public static User admin(String email, String name, String password, String organization, String phoneNumber) {
-        return new User(email, name, password, organization, phoneNumber, Role.ADMIN);
+    public static User admin(String email, String name, String password, String phoneNumber) {
+        return new User(email, name, password, null, phoneNumber, Role.ADMIN);
     }
 
     public void updateProperty(User user) {
         this.name = Objects.isNull(user.getEmail()) ? this.name : user.getName();
-        this.password = Objects.isNull(user.getPassword()) ? this.organization : user.getPassword();
+        this.password = Objects.isNull(user.getPassword()) ? this.password : user.getPassword();
         this.organization = Objects.isNull(user.getOrganization()) ? this.organization : user.getOrganization();
         this.phoneNumber = Objects.isNull(user.getPhoneNumber()) ? this.phoneNumber : user.getPhoneNumber();
     }
@@ -72,7 +75,7 @@ public class User extends UpdateBaseEntity {
         return password;
     }
 
-    public String getOrganization() {
+    public Organization getOrganization() {
         return organization;
     }
 
