065feb928f484b58c8834f0ae1454edd8ede8bf5
parkyountaek <yountaek11@gmail.com>
2023-05-14 21:10:40 +0900

    rename: 파일 위치 이동

--- borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/adapter/out/LoadUserCase.java
+++ borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/adapter/out/LoadUserCase.java
@@ -1,8 +1,9 @@
-package com.borrow.system.appusermanagement.application.port.out;
+package com.borrow.system.appusermanagement.adapter.out;
 
 import com.borrow.system.modulecore.user.domain.User;
 
 public interface LoadUserCase {
     User getUserById(Long id);
     User getUserByEmail(String email);
+    void existUserByEmail(String email);
 }
