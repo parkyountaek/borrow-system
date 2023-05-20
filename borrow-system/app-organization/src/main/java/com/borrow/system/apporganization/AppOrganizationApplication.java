324ee41e294ade8f723eea0233889e836c638229
parkyountaek <yountaek11@gmail.com>
2023-05-20 23:43:48 +0900

    feat: app-organization spring application

---
+++ borrow-system/app-organization/src/main/java/com/borrow/system/apporganization/AppOrganizationApplication.java
@@ -0,0 +1,20 @@
+package com.borrow.system.apporganization;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan(basePackages = {
+        "com.borrow.system.modulecore.organization.*"
+})
+@SpringBootApplication(scanBasePackages = {
+        "com.borrow.system.modulecore.*",
+        "com.borrow.system.modulecommon.*",
+        "com.borrow.system.apporganization.*"
+})
+public class AppOrganizationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppOrganizationApplication.class, args);
+    }
+}
