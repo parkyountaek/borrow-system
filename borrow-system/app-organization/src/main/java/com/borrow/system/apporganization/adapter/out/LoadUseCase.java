b335bafe322ca00de00adaa5f07a8cc0d79dce88
parkyountaek <yountaek11@gmail.com>
2023-05-20 23:41:16 +0900

    feat: persistence port

--- borrow-system/app-organization/src/main/java/com/borrow/system/apporganization/adapter/out/LoadUseCase.java
+++ borrow-system/app-organization/src/main/java/com/borrow/system/apporganization/adapter/out/LoadUseCase.java
@@ -1,2 +1,9 @@
-package com.borrow.system.apporganization.adapter.out;public interface LoadUseCase {
+package com.borrow.system.apporganization.adapter.out;
+
+import com.borrow.system.modulecore.organization.domain.Organization;
+
+import java.util.List;
+
+public interface LoadUseCase {
+    List<Organization> getAllByName(String name);
 }
