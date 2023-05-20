5f1037ca220f4c073d46eca21bc0bfa7d84acd52
parkyountaek <yountaek11@gmail.com>
2023-05-20 23:42:18 +0900

    feat: application port

--- borrow-system/app-organization/src/main/java/com/borrow/system/apporganization/application/port/in/OrganizationUpdateUseCase.java
+++ borrow-system/app-organization/src/main/java/com/borrow/system/apporganization/application/port/in/OrganizationUpdateUseCase.java
@@ -1,2 +1,7 @@
-package com.borrow.system.apporganization.application.port.in;public interface OrganizationUpdateUseCase {
+package com.borrow.system.apporganization.application.port.in;
+
+import com.borrow.system.modulecore.organization.domain.Organization;
+
+public interface OrganizationUpdateUseCase {
+    Organization updateOrganization(Organization organization);
 }
