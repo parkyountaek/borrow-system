82217e72ff32b9a6ff8fdb618f1cf434051d3427
parkyountaek <yountaek11@gmail.com>
2023-05-20 23:36:28 +0900

    feat: app-organization 추가에 따른 scan 패키지 추가

--- borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/AppUserManagementApplication.java
+++ borrow-system/app-user-management/src/main/java/com/borrow/system/appusermanagement/AppUserManagementApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(basePackages = {"com.borrow.system.modulecore.user.*"})
+@EntityScan(basePackages = {
+        "com.borrow.system.modulecore.user.*",
+        "com.borrow.system.modulecore.organization.*"
+})
 @SpringBootApplication(scanBasePackages = {
         "com.borrow.system.modulecore.*",
         "com.borrow.system.modulecommon.*",
-        "com.borrow.system.appusermanagement.*"
+        "com.borrow.system.appusermanagement.*",
+        "com.borrow.system.apporganization.*"
+
 })
 public class AppUserManagementApplication {
     public static void main(String[] args) {
